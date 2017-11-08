6e35a6098585d26547350e348679f4e72119a617
miczal <miczal19@gmail.com>
2017-11-08 21:36:07 +0100

    add mobile mode to menu

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,31 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+  viewMenu = false;
+  mobileMode = false;
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+    this.checkMobileMode(window.innerWidth);
+    this.route.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.viewMenu = false;
+      }
+    });
+  }
+
+  checkMobileMode(width) {
+    if ( width > 680) {
+      this.viewMenu = false;
+      this.mobileMode = false;
+    } else {
+      this.mobileMode = true;
+    }
+  }
+
+  checkActiveMenu() {
+    return this.viewMenu === true ? 'active' : 'inactive';
   }
 
 }
