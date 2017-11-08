01e94b0218ad35991f3d681b0fdd47cfc9c1e21b
miczal <miczal19@gmail.com>
2017-11-08 18:50:52 +0100

    create main structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './core/home/home.component';
+import { MonstersComponent } from './monsters/monsters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MonstersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/monsters/monsters.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-monsters',
+  templateUrl: './monsters.component.html',
+  styleUrls: ['./monsters.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MonstersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
